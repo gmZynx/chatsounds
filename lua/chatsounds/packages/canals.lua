4a0440546941fd3b85ad5a9b5903311e4697ffa6
zynx <52384539+gmZynx@users.noreply.github.com>
2021-02-21 17:06:47 +0200

    canals

---
+++ lua/chatsounds/packages/canals.lua
@@ -0,0 +1,132 @@
+local zcs_hl2_canals = {
+
+    {
+        ["its gonna be tough dodging civil protection out there in the open but drive hard and youll make it"] = {"vo/canals/airboat_drivehard.wav"},
+        ["hey doctor freeman id been hoping youd show up got this airboat all gassed up and ready to go"] = {"vo/canals/airboat_gassed.wav"},
+        ["youd better get going now"] = {"vo/canals/airboat_go_nag01.wav"},
+        ["i gotta stay here there could always be more runaways coming along"] = {"vo/canals/airboat_go_nag02.wav"},
+        ["im staying put you go on"] = {"vo/canals/airboat_go_nag03.wav"},
+        ["go ahead and get in the boat doctor freeman"] = {"vo/canals/airboat_nag01.wav"},
+        ["get in the airboat sir"] = {"vo/canals/airboat_nag02.wav"},
+        ["climb on in and fire her up"] = {"vo/canals/airboat_nag03.wav"},
+        ["youll find more help up at station seven just a few bins up the river its the old red barn"] = {"vo/canals/airboat_redbarn.wav"},
+    },
+
+    { ["black mesa east to station six do you read doctor freeman is on his way down river lend him the airboat and give him all the help you can repeat gordon freeman has returned it is critical he reaches black mesa east"] = {"vo/canals/al_radio_stn6.wav"}, },
+
+    {
+        ["get going"] = {"vo/canals/arrest_getgoing.wav"},
+        ["help me"] = {"vo/canals/arrest_helpme.wav"},
+        ["theyll be looking for you now"] = {"vo/canals/arrest_lookingforyou.wav"},
+        ["youd better run theres nothing else you can do here"] = {"vo/canals/arrest_run.wav"},
+        ["stop we didnt do anything"] = {"vo/canals/arrest_stop.wav"},
+    },
+
+    {
+        ["be careful now"] = {"vo/canals/boxcar_becareful.wav"},
+        ["we really cant afford to get noticed"] = {"vo/canals/boxcar_becareful_b.wav"},
+        ["civil protection catches you down here its bad news for the whole railroad"] = {"vo/canals/boxcar_becareful_c.wav"},
+        ["get going now"] = {"vo/canals/boxcar_go_nag01.wav"},
+        ["better get going"] = {"vo/canals/boxcar_go_nag02.wav"},
+        ["get outta here"] = {"vo/canals/boxcar_go_nag03.wav"},
+        ["hit the road jack"] = {"vo/canals/boxcar_go_nag04.wav"},
+        ["meanwhile let my vortigaunt friend here give you a jolt to get you going"] = {"vo/canals/boxcar_jolt.wav"},
+        ["he will help you if you let him"] = {"vo/canals/boxcar_lethimhelp.wav"},
+        ["look were just a lookout for the underground railroad"] = {"vo/canals/boxcar_lookout.wav"},
+        ["main stations right around the corner"] = {"vo/canals/boxcar_lookout_b.wav"},
+        ["theyll get you started on the right foot"] = {"vo/canals/boxcar_lookout_d.wav"},
+        ["guess those sirens are from you huh"] = {"vo/canals/boxcar_sirens.wav"},
+        ["good thing you found us"] = {"vo/canals/boxcar_sirens_b.wav"},
+        ["youre not the first to come through here by al"] = {"vo/canals/boxcar_sirens_c.wav"},
+        ["hey stop that the vorts are on our side now"] = {"vo/canals/boxcar_vortstop.wav"},
+    },
+
+    {
+        ["come on in ill show you what youre up against"] = {"vo/canals/gunboat_comein.wav"},
+        ["this here is the dam its just up ahead"] = {"vo/canals/gunboat_dam.wav"},
+        ["i think hes just finishing up now"] = {"vo/canals/gunboat_finishingup.wav"},
+        ["come on get in here"] = {"vo/canals/gunboat_getin.wav"},
+        ["go on out"] = {"vo/canals/gunboat_goonout.wav"},
+        ["here take a look at this"] = {"vo/canals/gunboat_herelook.wav"},
+        ["hey youre freeman arent you"] = {"vo/canals/gunboat_heyyourefm.wav"},
+        ["elis hideout is here a stones throw from the apron and nestled in the old hydro plant"] = {"vo/canals/gunboat_hideout.wav"},
+        ["but getting there with that hunter chopper on your ass next to impossible"] = {"vo/canals/gunboat_impossible.wav"},
+        ["there we are that gun came off one of the same hunter choppers youre up against"] = {"vo/canals/gunboat_irony.wav"},
+        ["good news is the vortigaunts working his magic on your airboat so youll have a little more firepower going forward"] = {"vo/canals/gunboat_magic.wav"},
+        ["take that chopper down and you should be able to tear on through to elis place"] = {"vo/canals/gunboat_takeitdown.wav"},
+        ["dont worry about the airboat vorts gonna look after it for you"] = {"vo/canals/gunboat_vort.wav"},
+    },
+    
+    {
+        ["be glad youre not the guy theyre looking for"] = {"vo/canals/matt_beglad.wav"},
+        ["poor bastard doesnt stand a chance"] = {"vo/canals/matt_beglad_b.wav"},
+        ["sounds like theyre calling in every cp unit in city 17"] = {"vo/canals/matt_beglad_c.wav"},
+        ["that was a close call thanks for your help"] = {"vo/canals/matt_closecall.wav"},
+        ["now theyre flooding the areas up ahead with manhacks"] = {"vo/canals/matt_flood.wav"},
+        ["youd better get going before they sweep through here"] = {"vo/canals/matt_flood_b.wav"},
+        ["get in here hurry"] = {"vo/canals/matt_getin.wav"},
+        ["youd better get going now"] = {"vo/canals/matt_go_nag01.wav"},
+        ["thanks but ill be okay got some work to finish up here"] = {"vo/canals/matt_go_nag02.wav"},
+        ["you understand ive gotta stay and help any stragglers"] = {"vo/canals/matt_go_nag03.wav"},
+        ["get going"] = {"vo/canals/matt_go_nag04.wav"},
+        ["you go on"] = {"vo/canals/matt_go_nag05.wav"},
+        ["good luck out there"] = {"vo/canals/matt_goodluck.wav"},
+        ["look it isnt safe here ill give you some supplies to keep you going and you really gotta go"] = {"vo/canals/matt_supplies.wav"},
+        ["civil protection is onto us"] = {"vo/canals/matt_tearinguprr.wav"},
+        ["were tearing up the railroad covering up our tracks"] = {"vo/canals/matt_tearinguprr_a.wav"},
+        ["looks like youre gonna be the last one through"] = {"vo/canals/matt_tearinguprr_b.wav"},
+        ["hey look thanks for the help but you better get outta here"] = {"vo/canals/matt_thanksbut.wav"},
+        ["oh shit too late"] = {"vo/canals/matt_toolate.wav"},
+    },
+
+    { ["wait dont what are ya doing please dont shoot dont shoot dont sho no dont shoot"] = {"vo/canals/premassacre.wav"}, },
+
+    {
+        ["station twelve come in station twelve do you read"] = {"vo/canals/radio_comein12.wav"},
+        ["station eight do you copy station eight are you there we have confirmed reports of manhacks repeat theyre filling the underground with manhacks"] = {"vo/canals/radio_doyoucopy8.wav"},
+        ["this is station eight we heard twelve go down and out surgical strike units are targetting railway stations repeat civil protection is coming down on underground stations were already getting refugees from nine and outlying looks like were"] = {"vo/canals/radio_thisis8.wav"},
+    },
+
+    {
+        ["you got here at a bad time"] = {"vo/canals/shanty_badtime.wav"},
+        ["get outta here"] = {"vo/canals/shanty_go_nag01.wav"},
+        ["go on"] = {"vo/canals/shanty_go_nag02.wav"},
+        ["get going"] = {"vo/canals/shanty_go_nag03.wav"},
+        ["weve got some ammo in those crates over there"] = {"vo/canals/shanty_gotsomeammo.wav"},
+        ["we got word you were coming"] = {"vo/canals/shanty_gotword.wav"},
+        ["help yourself to supplies and keep moving"] = {"vo/canals/shanty_helpyourself.wav"},
+        ["hey"] = {"vo/canals/shanty_hey.wav"},
+        ["youre freeman arent you"] = {"vo/canals/shanty_yourefm.wav"},
+    },
+
+    {
+        ["im gonna stay here incase any others come through gotta keep the railway alive"] = {"vo/canals/stn1_cit_illstay.wav"},
+        ["keep going friend that station was raided but theres others up ahead"] = {"vo/canals/stn1_cit_keepgoing.wav"},
+    },
+
+    { ["this is the freeman the combines reckoning has come"] = {"vo/canals/vort_reckoning.wav"}, },
+
+    {
+        ["get going doc weve gotta break camp"] = {"vo/canals/female01/gunboat_breakcamp.wav"},
+        ["get going doctor freeman i can see eli was right about you"] = {"vo/canals/female01/gunboat_eliright.wav"},
+        ["farewell doctor freeman"] = {"vo/canals/female01/gunboat_farewell.wav"},
+        ["give em hell doc"] = {"vo/canals/female01/gunboat_giveemhell.wav"},
+        ["wed better hurry weve gotta tear this camp down and get outta here"] = {"vo/canals/female01/gunboat_hurry.wav"},
+        ["you made it just in time weve gotta clear outta here before were discovered"] = {"vo/canals/female01/gunboat_justintime.wav"},
+        ["lets get a move on we gotta clear out before they target us"] = {"vo/canals/female01/gunboat_moveon.wav"},
+        ["well i wouldnt believe if i couldnt see it with my own eyes"] = {"vo/canals/female01/gunboat_owneyes.wav"},
+        ["you can park the boat itll be safe here"] = {"vo/canals/female01/gunboat_parkboat.wav"},
+        ["youre just in time doc weve gotta move out before the combine picks us up were just getting ready to pull out"] = {"vo/canals/female01/gunboat_pullout.wav"},
+    },
+
+    {
+        ["go on"] = {"vo/canals/female01/stn6_go_nag02.wav"},
+        ["incoming"] = {"vo/canals/female01/stn6_incoming.wav"},
+        ["theyre shelling us"] = {"vo/canals/female01/stn6_shellingus.wav"},
+    },
+
+}
+
+for _,pack in pairs( zcs_hl2_canals ) do
+    chatsounds:Mount( pack )
+end
